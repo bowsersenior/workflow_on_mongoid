d28b0727aeb16c70ab8692870fc95e99110dc492
Mani Tadayon <bowsersenior@gmail.com>
2011-09-19 19:56:17 -0700

    Bump version to 0.8.0.5

--- lib/workflow_on_mongoid/version.rb
+++ lib/workflow_on_mongoid/version.rb
@@ -2,5 +2,5 @@
 module WorkflowOnMongoid #:nodoc
   # major version number matches the latest supported version of the Workflow gem
   # minor version number for tracking changes to workflow_on_mongoid itself
-  VERSION = "0.8.0.4"
+  VERSION = "0.8.0.5"
 end
